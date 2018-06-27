ff895c111a04c6ec86be97c9784d4309c99c4d5e
Gábor Csárdi <csardi.gabor@gmail.com>
2018-06-27 11:00:02 +0100

    Fix error messages

--- R/error.R
+++ R/error.R
@@ -3,7 +3,7 @@ ps__access_denied <- function(pid = NULL, name = NULL) {
   pid <- pid %||% "???"
   name <- name %||% "???"
   structure(
-    list(message = sprintf("Permission denied: %s (pid %i)", pid, name),
+    list(message = sprintf("Permission denied: %s (pid %i)", name, pid),
          errno = 0),
     class = c("access_denied", "ps_error", "error", "condition"))
 }
@@ -13,7 +13,7 @@ ps__no_such_process <- function(pid = NULL, name = NULL) {
   name <- name %||% "???"
   structure(
     list(
-      message = sprintf("No such process: %s (pid %i)", pid, name),
+      message = sprintf("No such process: %s (pid %i)", name, pid),
       errno = 0),
     class = c("no_such_process", "ps_error", "error", "condition"))
 }
