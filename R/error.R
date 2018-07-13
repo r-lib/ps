046cee36ffcb03eab440b0a0ca91aee986128cec
Gábor Csárdi <csardi.gabor@gmail.com>
2018-07-13 01:10:54 +0100

    Remove most of the R error classes
    They are not used from R any more, only from C.

--- R/error.R
+++ R/error.R
@@ -1,39 +1,4 @@
 
-ps__access_denied <- function(pid = NULL, name = NULL) {
-  pid <- as.character(pid %||% "???")
-  name <- name %||% "???"
-  structure(
-    list(message = sprintf("Permission denied: %s (pid %s)", name, pid),
-         errno = 0),
-    class = c("access_denied", "ps_error", "error", "condition"))
-}
-
-ps__no_such_process <- function(pid = NULL, name = NULL) {
-  pid <- as.character(pid %||% "???")
-  name <- name %||% "???"
-  structure(
-    list(
-      message = sprintf("No such process: %s (pid %s)", name, pid),
-      errno = 0),
-    class = c("no_such_process", "ps_error", "error", "condition"))
-}
-
-ps__not_implemented <- function() {
-  structure(list(
-    message = "Not implemented on this platform", errno = 0),
-    class = c("not_implemented", "ps_error", "error",  "condition"))
-}
-
-ps__zombie_process <- function(pid = NULL, name = NULL, ppid = NULL) {
-  pid <- as.character(pid %||% "???")
-  name <- name %||% "???"
-  ppid <- as.character(ppid %||% "???")
-  structure(
-    list(message = sprintf("Zombie process: %s (pid %s, parent: %s)",
-                           name, pid, ppid), errno = 0),
-    class = c("zombie_process", "ps_error", "error", "condition"))
-}
-
 ps__package_not_available <- function(pkg, func = NULL) {
   structure(
     list(message = paste0(
