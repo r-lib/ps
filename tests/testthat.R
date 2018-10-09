09dfdd1ae966f42cd01b1cdb45a5119e1f71e4a4
Gábor Csárdi <csardi.gabor@gmail.com>
2018-10-09 23:19:46 +0100

    No cleanup reporter for covr run

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(ps)
 
-if (ps::ps_is_supported()) {
+if (ps::ps_is_supported() && Sys.getenv("R_COVR", "") != "true") {
   reporter <- ps::CleanupReporter(testthat::SummaryReporter)$new()
 } else {
   ## ps does not support this platform
