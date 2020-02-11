339aa42c2886c0607cf103c7fcaa4ef6b9d38f12
Gábor Csárdi <csardi.gabor@gmail.com>
2020-02-11 10:27:02 +0000

    Do not use cleanup reporter on CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,8 @@
 library(testthat)
 library(ps)
 
-if (ps::ps_is_supported() && Sys.getenv("R_COVR", "") != "true") {
+if (ps::ps_is_supported() && Sys.getenv("R_COVR", "") != "true" &&
+    Sys.getenv("NOT_CRAN") != "" ) {
   reporter <- ps::CleanupReporter(testthat::SummaryReporter)$new()
 } else {
   ## ps does not support this platform
