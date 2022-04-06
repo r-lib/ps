61527ec7fc9047d222b700774b588b4269a24244
Gábor Csárdi <csardi.gabor@gmail.com>
2022-04-06 17:45:45 +0200

    Remove outdated `testthat.R` comment

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,7 +5,6 @@ if (ps::ps_is_supported() && Sys.getenv("R_COVR", "") != "true" &&
     Sys.getenv("NOT_CRAN") != "" ) {
   reporter <- ps::CleanupReporter(testthat::SummaryReporter)$new()
 } else {
-  ## ps does not support this platform
   reporter <- "summary"
 }
 
