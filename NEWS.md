d6da9904f836ab7f33e983088e0689cd6a7a77e7
Gábor Csárdi <csardi.gabor@gmail.com>
2025-02-18 14:31:47 +0100

    Allow selecting columns in `ps()`
    Closes #138.

--- NEWS.md
+++ NEWS.md
@@ -3,6 +3,9 @@
 * `ps_memory_full_info()` now contains `maxrss`, the maximum resident set
   size for the calling process.
 
+* New `columns` argument in `ps()`, to customize what data is returned
+  (#138).
+
 # ps 1.8.1
 
 * ps can now be installed again on unsupported platforms.
--- tests/testthat/test-ps.R
+++ tests/testthat/test-ps.R
@@ -4,3 +4,9 @@ test_that("issue #129", {
   pss <- ps(user = "root", after = as.POSIXct('2022-05-15', tz = "GMT"))
   expect_s3_class(pss, "tbl")
 })
+
+test_that("can select columns", {
+  skip_on_cran()
+  expect_silent(ps(user = ps_username(), columns = c("pid", "username")))
+  expect_silent(ps(user = ps_username(), columns = "*"))
+})
